509f568a2c984f4296569dab3e2b6b8accedaba3
aininurulazizah <aini.nurul.tif421@polban.ac.id>
2024-05-18 00:10:38 +0700

    Add generating report

--- src/test/java/CucumberTestRunner.java
+++ src/test/java/CucumberTestRunner.java
@@ -3,6 +3,6 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"html:target/test-report.html"}, features = "src/test/resources/features")
+@CucumberOptions(plugin = {"html:reports/test-report.html"}, features = "src/test/resources/features")
 public class CucumberTestRunner {
 }
